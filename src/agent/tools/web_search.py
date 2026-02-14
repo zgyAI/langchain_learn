04b8595c00465d26b1f28e47273807ec4f3212ae
zgy <1263730840@qq.com>
2026-02-14 12:14:31 +0800

    搜索工具完善

--- src/agent/tools/web_search.py
+++ src/agent/tools/web_search.py
@@ -1,19 +1,19 @@
 from langchain_core.tools import tool
-from agent.my_llm import openai_llm
-from langchain_tavily import TavilySearch
+from openai import api_key
+from langchain_community.tools.tavily_search import TavilySearchResults
 import os
 
-tavily = TavilySearch(api_key=os.environ.get("TAVILY_API_KEY"), max_results=5)
+tavily = TavilySearchResults(
+    api_key=os.environ.get("TAVILY_API_KEY"),
+    max_results=5)
 
-@tool("web_search", parse_docstring=True)
+# @tool("web_search", parse_docstring=True)
+@tool
 def web_search(query: str) -> str:
     """
     互联网搜索工具，可以搜索所有的信息
-
     Args:
         query: 需要进行互联网查询的信息
-
-
     Returns:
         返回搜索的结果信息，该信息
     """
@@ -21,15 +21,13 @@ def web_search(query: str) -> str:
         res = tavily.invoke(query)
         if not res:
             return "没有搜索结果"
-        # if isinstance(res, str):
-        #     return res
-        # blocks = []
-        # for i, r in enumerate(res, 1):
-        #     page_title = r.get('title',"")
-        #     url = r.get('url',"")
-        #     content = r.get('content',"") or r.get("snippet", "")
-        #     blocks.append(f"{i}.{page_title}\n{url}\n{content}\n".strip())
-        # return "\n\n".join(blocks)
+        blocks = []
+        for i, r in enumerate(res, 1):
+            page_title = r.get('title',"")
+            url = r.get('url',"")
+            content = r.get('content',"") or r.get("snippet", "")
+            blocks.append(f"{i}.{page_title}\n{url}\n{content}")
+        return "\n\n".join(blocks)
 
     except Exception as e:
         print(e)
